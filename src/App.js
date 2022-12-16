baa95158c9d9bf6cd24176648e345bb5617b6c20
nicholascarlin <54487246+nicholascarlin@users.noreply.github.com>
2022-12-16 12:05:41 -0600

    Progress

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { CurrencyList } from './components/utils/CurrencyList';
 import { GetAllAvailableCurrencies } from './components/data/APICalls';
+import { getEmojiByCurrencyCode } from 'country-currency-emoji-flags';
 
 function App() {
 	return (
-		<div className='App'>
-			<div>test</div>
-			<button
-				onClick={() => {
-					GetAllAvailableCurrencies();
-				}}
-				className='bg-red-500'>
-				Get All
-			</button>
+		<div className='w-screen h-screen'>
+			<h1>Currency Hub</h1>
+			<hr />
+			<h3>Compare Currencies of Your Choice</h3>
+			<div className='flex'></div>
+			{/* <div>
+				{CurrencyList.map((item, idx) => {
+					return (
+						<div className='flex space-x-2' key={idx}>
+							<div>{item.name}</div>
+							<div>{item.code}</div>
+							<div>{getEmojiByCurrencyCode(item.code) || 'NULL'}</div>
+							<div>{item.symbol}</div>
+						</div>
+					);
+				})}
+			</div> */}
 		</div>
 	);
 }
